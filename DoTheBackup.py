da4049a745c737de179f861edd8c45ce8360b22c
Marvin Steadfast <marvin@xsteadfastx.org>
2014-10-16 16:33:31 +0200

    complete rewrite

--- DoTheBackup.py
+++ DoTheBackup.py
@@ -1,62 +1,93 @@
 '''
 Usage:
-    DoTheBackup.py do <from> <to>
-    DoTheBackup.py do -f <file>
+    DoTheBackup.py (-f <config> | --file <config>)
 
 Options:
-    do          syncs dir to dir
-    do -f       reads backup dirs from file
+    -f --file        Reads config from YAML File
 '''
-import datetime
+import arrow
+import yaml
+import os
 import subprocess
-import os.path
 from docopt import docopt
 
 
-arguments = docopt(__doc__)
+ARGUMENTS = docopt(__doc__)
 
-TODAY = datetime.date.today().strftime('%d')
-YESTERDAY = (datetime.date.today() - datetime.timedelta(1)).strftime('%d')
-
-
-def do(backup_from, backup_to):
-    backup_from = os.path.abspath(os.path.normpath(backup_from))
-    backup_to_today = os.path.join(os.path.abspath(os.path.normpath(backup_to)), TODAY)
-    link_dest = os.path.join(os.path.abspath(os.path.normpath(backup_to)), YESTERDAY)
-
-    cmd = ['rsync', '-av', '--delete', '--link-dest=' + link_dest, backup_from,
-           backup_to_today]
-
-    if not os.path.exists(backup_to_today):
-        os.makedirs(backup_to_today)
-
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)
-
-    with open(backup_to + '/' + os.path.basename(backup_from) + '.log', 'w') as f:
-        for i in proc.stdout:
-            f.write(i)
-        proc.wait()
-        f.write('Exit Code: ' + str(proc.returncode))
+NOW = arrow.utcnow()
+TODAY = NOW.format('DD')
+YESTERDAY = NOW.replace(days=-1).format('DD')
 
 
-def do_filelist():
-    filelist = os.path.abspath(os.path.normpath(arguments['<file>']))
+def rsync_cmd(config):
+    source = os.path.abspath(os.path.normpath(config['source'])) + '/'
+    destination = os.path.abspath(os.path.normpath(config['destination']))
 
-    with open(filelist) as f:
-        for i in f.readlines():
-            backup_from = os.path.abspath(i.split()[0])
-            backup_to = os.path.abspath(i.split()[1])
+    # adding basic rsync stuff
+    cmd = ['rsync', '-av', '--delete']
 
-            do(backup_from, backup_to)
+    # check for exclude and include and add it to the rsync cmd list
+    if 'exclude' in config:
+        for sequence in config['exclude']:
+            cmd.append('--exclude={}'.format(sequence))
+    if 'include' in config:
+        for sequence in config['include']:
+            cmd.append('--include={}'.format(sequence))
+
+    # do magic of type is month
+    if config['type'] == 'month':
+        cmd.append('--link-dest={}'.format(
+            os.path.join(destination, YESTERDAY)))
+        destination = os.path.join(destination, TODAY)
+    elif config['type'] == 'once':
+        pass
+
+    # adding source and destination to the rsync cmd list
+    cmd.append(source)
+    cmd.append(destination)
+
+    # returns complete cmd list
+    return cmd
+
+
+def from_file():
+    # load config file
+    with open(ARGUMENTS['<config>']) as f:
+        config = yaml.load(f)
+
+    # work through config file
+    for scalar, sequence in config['backup'].viewitems():
+        # get destination out of config
+        destination = os.path.abspath(
+            os.path.normpath(sequence['destination']))
+
+        # create backup dir if not there
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+
+        # create process
+        proc = subprocess.Popen(rsync_cmd(sequence),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
+
+        # get abspath of log_dir
+        log_dir = os.path.abspath(os.path.normpath(config['log_dir']))
+
+        # create log_dir if not there
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        # write logfile
+        with open(os.path.join(log_dir, '{}.log'.format(scalar)), 'w') as f:
+            for line in proc.stdout:
+                f.write(line)
+            proc.wait()
+            f.write('Exit Code: {}'.format(str(proc.returncode)))
 
 
 def main():
-    if arguments['do']:
-        if arguments['-f']:
-            do_filelist()
-        else:
-            do(arguments['<from>'], arguments['<to>'])
+    if '--file' or '-f' in ARGUMENTS:
+        from_file()
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,14 +1,56 @@
 DoTheBackup
 ===========
 
-Backups files in a directory names after the backup day. It tries to use hardlinks from the day before to reduce space. You can also use a list of dirs as input. 
+a little python script for handling my backups. if `type: month` its using hardlinks between days to save diskspace.
 
-```
-Usage:
-    DoTheBackup.py do <from> <to>
-    DoTheBackup.py do -f <file>
+## usage
+	Usage:
+		DoTheBackup.py (-f <config> | --file <config>)
 
-Options:
-    do          syncs dir to dir
-    do -f       reads backup dirs from file
-```
+	Options:
+		-f --file        Reads config from YAML File
+
+## config
+	# destination where the logs will be
+	log_dir: /var/log/DoTheBackup
+	# define your backups here
+	backup:
+		my_documents:
+			# "month" means that it will save the backup in daily directories
+			# for example: "/media/backup/documents/07"
+			type: month
+			source: /home/user/documents
+			destination: /media/backup/documents
+			# rsync --exclude patterns here
+			exclude:
+				- foo
+				- bar
+			# rsync --include patterns here
+			include:
+				- very_important_dir
+		video:
+			# "once" backups straight in the destination directory
+			# for example: "/media/backup/Videos"
+			type: once
+			source: /home/user/Media/Videos
+			destination: /media/backup/Videos
+
+## install
+for example in /opt
+
+1. `cd /opt`
+2. `git clone https://github.com/xsteadfastx/DoTheBackup.git`
+3. `cd DoTheBackup`
+4. `virtualenv venv`
+5. `source venv/bin/activate`
+6. `pip install -r requirements.txt`
+7. `cp DoTheBackup.yaml.dist DoTheBackup.yaml`
+8. `vim DoTheBackup.yaml`
+
+## running in crontab
+i use a shell wrapper script for handling the virtualenv
+
+1. `/opt/DoTheBackup`
+2. `cp DoTheBackup-cron.sh.dist DoTheBackup-cron.sh`
+3. `vim DoTheBackup-cron.sh`
+4. edit your crontab and add `/opt/DoTheBackup/DoTheBackup-cron.sh`
