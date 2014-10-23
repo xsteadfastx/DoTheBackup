f431d47263840279dec9dd2b19ede1833d71e44d
Marvin Steadfast <marvin@xsteadfastx.org>
2014-10-23 13:00:59 +0200

    fixed a bug with the paths when running the tests from a different location

--- test.py
+++ test.py
@@ -7,25 +7,57 @@ from uuid import uuid4
 from DoTheBackup import from_file
 
 
-def inode_list(filelist):
-    inodes = []
-    for f in filelist:
-        fd = os.open(f, os.O_RDONLY)
-        info = os.fstat(fd)
-        inodes.append(info.st_ino)
-
-    return inodes
-
-
 class TestDoTheBackup(unittest.TestCase):
+    @staticmethod
+    def inode_list(filelist):
+        ''' returns a list of inodes for files '''
+        inodes = []
+        for f in filelist:
+            fd = os.open(f, os.O_RDONLY)
+            info = os.fstat(fd)
+            inodes.append(info.st_ino)
+
+        return inodes
+
     def setUp(self):
-        # create some time variables
+        # create some variables
+        self.test_dir = os.path.dirname(os.path.abspath(__file__))
+
         self.now = arrow.utcnow()
         self.today = self.now.format('DD')
         self.yesterday = self.now.replace(days=-1).format('DD')
 
+        # create yaml
+        test_data = {
+            'log_dir': os.path.join(
+                self.test_dir,
+                'tmp-test/logs'),
+            'backup': {
+                'test-rsync-month': {
+                    'type': 'rsync_month',
+                    'enabled': 'true',
+                    'source': os.path.join(
+                        self.test_dir,
+                        'tmp-test/backup/rsync-month-source'),
+                    'destination': os.path.join(
+                        self.test_dir,
+                        'tmp-test/backup/rsync-month-destination')},
+                'test-rsync-once': {
+                    'type': 'rsync_once',
+                    'enabled': 'true',
+                    'source': os.path.join(
+                        self.test_dir,
+                        'tmp-test/backup/rsync-once-source'),
+                    'destination': os.path.join(
+                        self.test_dir,
+                        'tmp-test/backup/rsync-once-destination')}}}
+
+        # save yaml
+        with open(os.path.join(self.test_dir, 'test.yaml'), 'w') as f:
+            f.write(yaml.dump(test_data))
+
         # get config from test.yaml
-        with open('test.yaml') as f:
+        with open(os.path.join(self.test_dir, 'test.yaml')) as f:
             self.config = yaml.load(f)
 
         # create dirs and fake files
@@ -42,15 +74,22 @@ class TestDoTheBackup(unittest.TestCase):
             os.makedirs(sequence['destination'])
 
         self.arguments = {}
-        self.arguments['<config>'] = 'test.yaml'
+        self.arguments['<config>'] = os.path.join(self.test_dir, 'test.yaml')
         self.arguments['--verbose'] = False
 
         # run from_file to do the backup magic
         from_file(self.arguments)
 
     def tearDown(self):
+        # delete test.yaml
+        os.remove(os.path.join(
+            self.test_dir,
+            'test.yaml'))
+
         # delete tmp test folders
-        shutil.rmtree('tmp-test')
+        shutil.rmtree(os.path.join(
+            self.test_dir,
+            'tmp-test'))
 
     def test_file_list(self):
         ''' test if source filelist is equal to destination filelist '''
@@ -82,17 +121,14 @@ class TestDoTheBackup(unittest.TestCase):
                 # create inode list for the today backup dir
                 today_dir = os.path.join(destination, self.today)
                 today_filelist = [os.path.join(today_dir, f) for f in os.listdir(today_dir)]
-                today_inodes = inode_list(today_filelist)
+                today_inodes = self.inode_list(today_filelist)
 
                 # create inode list for the yesterday backup dir
                 yesterday_dir = os.path.join(destination, self.yesterday)
                 yesterday_filelist = [os.path.join(yesterday_dir, f) for f in os.listdir(yesterday_dir)]
-                yesterday_inodes = inode_list(yesterday_filelist)
+                yesterday_inodes = self.inode_list(yesterday_filelist)
 
                 self.assertEqual(today_inodes, yesterday_inodes)
-
-    def test_exit_codes(self):
-        pass
 
 
 if __name__ == '__main__':
